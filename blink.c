d610ef382675cd751a9211ef823d2dd9e907a9c3
Sean Silva <chisophugis@gmail.com>
2015-07-20 09:58:36 -0700

    Trivial UART

--- blink.c
+++ blink.c
@@ -12,19 +12,51 @@ void main(void)
 
     WDT_A->rCTL.r = WDTPW | WDTHOLD;    // Stop watchdog timer
 
+    // Map USCI_A0 to outputs.
+    PMAP->rKEYID = 0x2D52; // Enable writing.
+    // 11.2.2
+    // PxSEL.y
+    PMAP->rP3MAP45 = (PMAP->rP3MAP45 & 0xFF) | (PM_UCA0TXD << 8); // Ugly. Should use an array of bytes instead.
+    DIO->rPBSEL0.b.bP3SEL0 = BIT5;
+
     // The following code toggles P1.0 port
     DIO->rPADIR.b.bP1DIR |= BIT0;       // Configure P1.0 as output
     DIO->rPBDIR.b.bP3DIR |= BIT6;
 
-    // TODO:
-    // Assert CSn and wait for SO to go low (see section 19.1.1 of the cc1101 datasheet).
-    // Set FREND0.PA_POWER=0 and put the desired output power in index 0 of PATABLE
+    EUSCI_A0->rCTLW0.b.bSWRST = 1; // Reset USCI_A0.
+    // Configure ports.
+    //EUSCI_A0->rCTLW0.b.b
 
+    // Use SMCLK, 3MHz out of the box according to http://www.ti.com/lit/ug/slau597/slau597.pdf sec. 2.6
+    // 22.3.10
+    // 3,000,000/115200 = 26.0417
+    EUSCI_A0->rMCTLW.b.bOS16 = 1;
+    EUSCI_A0->rBRW = 1;
+    EUSCI_A0->rMCTLW.b.bBRF = 10;
+    EUSCI_A0->rMCTLW.b.bBRS = 0x00; // Table 22-4
 
-    while(1)
-    {
-        DIO->rPAOUT.b.bP1OUT ^= BIT0;   // Toggle P1.0
-        DIO->rPBOUT.b.bP3OUT ^= BIT6;
-        for(i=1000; i>0; i--);         // Delay
+    EUSCI_A0->rCTLW0.b.bSSEL = 2;
+
+    // ultimately: "Enable interrupts (optional) via UCRXIE or UCTXIE." - but busy loop for now.
+    EUSCI_A0->rCTLW0.b.bSWRST = 0; // Bring USCI_A0 out of reset.
+
+    static uint8_t msg[] = {0x55, 0xAA, 0x55, 0xAA};
+    for (;;) {
+    	++i;
+    	i &= 3;
+    	// This also clears TXIFG automatically.
+    	EUSCI_A0->rTXBUF.b.bTXBUF = msg[i];
+    	volatile int delay;
+    	for (delay = 0; delay < 1000; delay++) // How on earth is this delaying 6.6ms?
+    		continue;
+    	while (EUSCI_A0->rIFG.b.bTXIFG != 1)
+    		continue;
     }
+
+//    while(1)
+//    {
+//        DIO->rPAOUT.b.bP1OUT ^= BIT0;   // Toggle P1.0
+//        DIO->rPBOUT.b.bP3OUT ^= BIT6;
+//        for(i=1000; i>0; i--);         // Delay
+//    }
 }
