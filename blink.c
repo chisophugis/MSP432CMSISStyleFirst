b76054a36ff051877102417125dfb4faace16ff6
Sean Silva <chisophugis@gmail.com>
2015-07-20 11:26:17 -0700

    Some refactoring.
    I'm trying to get the hang of choosing a balance between
    device-specific names for things and the "semantic" names.
    Sometimes it makes sense to talk in terms of one, other times
    in terms of the other.
    
    I just need to get more experience.

--- blink.c
+++ blink.c
@@ -6,7 +6,8 @@
 
 #include "msp.h"
 
-// EUSCI_A0 - UART for console
+// EUSCI_A0 - 115200 baud UART for console
+void init_uart_115200(void);
 // EUSCI_A1 - SPI for talking to CC1101
 
 void main(void)
@@ -21,23 +22,9 @@ void main(void)
     // PxSEL.y
     PMAP->rP3MAP45 = (PMAP->rP3MAP45 & 0xFF) | (PM_UCA0TXD << 8); // Ugly. Should use an array of bytes instead.
     DIO->rPBSEL0.b.bP3SEL0 = BIT5;
+    // TODO: Map the RX port. (will need interrupts for sane "select"?)
 
-    EUSCI_A0->rCTLW0.b.bSWRST = 1; // Reset USCI_A0.
-    // Configure ports.
-    //EUSCI_A0->rCTLW0.b.b
-
-    // Use SMCLK, 3MHz out of the box according to http://www.ti.com/lit/ug/slau597/slau597.pdf sec. 2.6
-    // 22.3.10
-    // 3,000,000/115200 = 26.0417
-    EUSCI_A0->rMCTLW.b.bOS16 = 1;
-    EUSCI_A0->rBRW = 1;
-    EUSCI_A0->rMCTLW.b.bBRF = 10;
-    EUSCI_A0->rMCTLW.b.bBRS = 0x00; // Table 22-4
-
-    EUSCI_A0->rCTLW0.b.bSSEL = 2;
-
-    // ultimately: "Enable interrupts (optional) via UCRXIE or UCTXIE." - but busy loop for now.
-    EUSCI_A0->rCTLW0.b.bSWRST = 0; // Bring USCI_A0 out of reset.
+    init_uart_115200();
 
     static uint8_t msg[] = {0x55, 0xAA, 0x55, 0xAA};
     for (;;) {
@@ -51,4 +38,24 @@ void main(void)
     	while (EUSCI_A0->rIFG.b.bTXIFG != 1)
     		continue;
     }
+}
+
+void init_uart_115200(void)
+{
+    EUSCI_A0->rCTLW0.b.bSWRST = 1; // Reset USCI_A0.
+
+    // Use SMCLK, 3MHz out of the box according to http://www.ti.com/lit/ug/slau597/slau597.pdf sec. 2.6
+    // 22.3.10
+    // 3,000,000/115200 = 26.0417
+    EUSCI_A0->rMCTLW.b.bOS16 = 1;
+    EUSCI_A0->rBRW = 1;
+    EUSCI_A0->rMCTLW.b.bBRF = 10;
+    EUSCI_A0->rMCTLW.b.bBRS = 0x00; // Table 22-4
+
+    // Clock source selection.
+    EUSCI_A0->rCTLW0.b.bSSEL = 2;
+
+    // ultimately: "Enable interrupts (optional) via UCRXIE or UCTXIE." - but busy loop for now.
+
+    EUSCI_A0->rCTLW0.b.bSWRST = 0; // Bring USCI_A0 out of reset.
 }
