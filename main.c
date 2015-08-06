17c8ec04e24df9c5239938332350bdbcdc85b73c
Sean Silva <chisophugis@gmail.com>
2015-08-06 00:02:31 -0700

    Small cleanups to UART initialization.
    Also, add links to the primary MSP432 resources I'm looking at.

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 #include "msp.h"
 
+// # Useful documents
+//
+// MSP432P401x datasheet <http://www.ti.com/lit/gpn/msp432p401r>
+// MSP432P4xx Family TRM <http://www.ti.com/lit/pdf/slau356>
+// MSP‑EXP432P401R UG <http://www.ti.com/lit/pdf/slau597>
 
 // EUSCI_A0 - 115200 baud UART for console
 void init_uart_115200(void);
@@ -44,29 +49,24 @@ void init_uart_115200(void)
     // P3.5 - EUSCI_A0 TXD
     // P3.6 - EUSCI_A0 RXD
     PMAP->rKEYID = 0x2D52; // Enable writing.
-    // 11.2.2; PxSEL.y
     volatile uint8_t *p3 = (volatile uint8_t *)&PMAP->rP3MAP01;
     p3[5] = PM_UCA0TXD;
     p3[6] = PM_UCA0RXD;
     DIO->rPBSEL0.b.bP3SEL0 |= (BIT5 | BIT6);
 
     // Configure.
-    EUSCI_A0->rCTLW0.b.bSWRST = 1; // Reset USCI_A0.
+    EUSCI_A0->rCTLW0.b.bSWRST = 1; // Bring into reset.
 
-    // Use SMCLK, 3MHz out of the box according to http://www.ti.com/lit/ug/slau597/slau597.pdf sec. 2.6
-    // 22.3.10
+    // Use SMCLK, 3MHz out of the box for MSP‑EXP432P401R.
+    // See Family TRM Section 22.3.10 for the details of the baud rate setup here.
     // 3,000,000/115200 = 26.0417
     EUSCI_A0->rMCTLW.b.bOS16 = 1;
     EUSCI_A0->rBRW = 1;
     EUSCI_A0->rMCTLW.b.bBRF = 10;
     EUSCI_A0->rMCTLW.b.bBRS = 0x00; // Table 22-4
+    EUSCI_A0->rCTLW0.b.bSSEL = 2; // SMCLK clock source.
 
-    // Clock source selection.
-    EUSCI_A0->rCTLW0.b.bSSEL = 2; // SMCLK
-
-    // ultimately: "Enable interrupts (optional) via UCRXIE or UCTXIE." - but busy loop for now.
-
-    EUSCI_A0->rCTLW0.b.bSWRST = 0; // Bring USCI_A0 out of reset.
+    EUSCI_A0->rCTLW0.b.bSWRST = 0; // Bring out of reset.
 }
 
 void systick_wait(uint32_t nticks)
